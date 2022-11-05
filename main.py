aa197b6e5cf5d28281d0eb12ccf40b4f1519bcda
Daniel <mrpickles123115@gmail.com>
2022-11-05 13:05:12 -0600

    wow what a great wordsearch solver

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import os
+CONTENTS = "contents1.txt"
+WORDS = "search1.txt"
 unkowns = []
 words = []
-with open("contents.txt", 'r') as f:
+with open(CONTENTS, 'r') as f:
     contents = f.readline()
     while contents != "":
         unkowns.append(list(contents.replace("\n", "").lower()))
         contents = f.readline()
-with open("search.txt", 'r') as f:
+with open(WORDS, 'r') as f:
     contents = f.readline()
     while contents != "":
         words.append(contents.replace("\n", "").lower())
@@ -35,7 +37,7 @@ for y in range(height):
                     if not (word in wordsForward):
                         word = word[::-1]
                         forward = False
-                    solutions[word] = [(x, y), "Horizontal", forward]
+                    solutions[word] = [(x, y) if forward else (x + increment, y), "Horizontal", forward]
                     found = True
                 increment += 1
             found = False
@@ -48,7 +50,7 @@ for y in range(height):
                     if not (word in wordsForward):
                         word = word[::-1]
                         forward = False
-                    solutions[word] = [(x, y), "Down", forward]
+                    solutions[word] = [(x, y) if forward else (x, y + increment), "Down", forward]
                     found = True
                 increment += 1
             found = False
@@ -61,12 +63,27 @@ for y in range(height):
                     if not (word in wordsForward):
                         word = word[::-1]
                         forward = False
-                    solutions[word] = [(x, y), "Diagnol", forward]
+                    solutions[word] = [(x, y) if forward else (x + increment, y + increment), "Diagonal -1", forward]
+                    found = True
+                increment += 1
+            word = ""
+            increment = 0
+            while(y - increment < height and x + increment < width and (not found) and (len(word) < longestWord)):
+                word = word + (unkowns[y - increment][x + increment])
+                if word in words:
+                    forward = True
+                    if not (word in wordsForward):
+                        word = word[::-1]
+                        forward = False
+                    solutions[word] = [(x, y) if forward else (x + increment, y + increment), "Diagonal +1", forward]
                     found = True
                 increment += 1
                 
             #right
             #down
             #diagnol
+found = 0
 for key in solutions:
+    found += 1
     print("Word: {0} ---> {1}".format(key, solutions[key]))
+print(found)
